7cf8d4ccd46ce7586a8c0d8109bc4dd9ee962245
shunsugawara <p0921065@gmail.com>
2018-06-28 17:30:35 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/sugawara/shun/qa_app/Const.java
@@ -0,0 +1,10 @@
+package jp.techacademy.sugawara.shun.qa_app;
+
+public class Const {
+    public static final String UserPATH = "users";
+    public static final String ContentsPATH = "contents";
+    public static final String AnswersPATH = "answers";
+
+    public static final String NameKEY = "name";
+
+}
